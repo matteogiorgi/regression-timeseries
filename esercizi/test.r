5d78d30b089eb05582cb379d11a62155caeaad10
matteogiorgi <matteo.giorgi@protonmail.com>
2025-11-18 23:23:21 +0100

    add some comments and docs

---
+++ esercizi/test.r
@@ -0,0 +1,11 @@
+# Installa il pacchetto
+# install.packages("AmesHousing")
+
+# Carica il pacchetto
+library(AmesHousing)
+
+# Importa il dataset in un data frame
+ames <- make_ames()
+
+# Controlla le prime righe
+head(ames)
